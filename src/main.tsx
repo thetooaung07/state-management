950be04bf90a6e3952054583da0cd3d129756423
Thet Oo Aung <thetooaung07@gmail.com>
2023-05-04 14:15:50 +0630

    Before New Statemanagement

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { CustomHooksApp } from "./customHooks/CustomHooksApp.tsx";
 import "./index.css";
-import { ToDoApp } from "./todoReducerPractice/ToDoApp.tsx";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <ToDoApp />
+    <CustomHooksApp />
   </React.StrictMode>
 );
